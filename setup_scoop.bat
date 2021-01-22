4fa16b601def1d554478691805e59c6b6d0438ea
Michael Kohl <citizen428@gmail.com>
2021-01-22 14:20:41 +0700

    Add Scoop setup script

---
+++ setup_scoop.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+TITLE Setting up Scoop
+ECHO ================
+ECHO Installing Scoop
+ECHO ================
+CALL powershell -C "iwr -useb get.scoop.sh | iex"
+ECHO ==============
+ECHO Adding buckets
+ECHO ==============
+CALL scoop install 7zip git
+CALL scoop bucket add extras
+CALL scoop bucket add nerd-fonts
+CALL scoop bucket add versions
+ECHO ===================
+ECHO Installing packages
+ECHO ===================
+CALL scoop install^
+ aria2^
+ delta^
+ git^
+ lab^
+ neovim-nightly^
+ qbittorrent^
+ signal^
+ slack^
+ starship^
+ sysinternals^
+ telegram^
+ wiztree^
+ wslgit^
+ z^
+ zeal
+ECHO ================
+ECHO Installing fonts
+ECHO ================
+CALL scoop install sudo
+CALL sudo scoop install FiraCode-NF
+ECHO =======
+ECHO Summary
+ECHO =======
+CALL scoop list
